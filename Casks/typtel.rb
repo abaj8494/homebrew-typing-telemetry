6297a527a11963fc4511236e1437bd53564a436e
Aayush Bajaj <33042478+abaj8494@users.noreply.github.com>
2026-01-10 03:40:01 +1100

    chore: bump cask to v1.1.7

--- Casks/typtel.rb
+++ Casks/typtel.rb
@@ -1,26 +1,22 @@
 cask "typtel" do
-  version "1.1.6"
-  sha256 "198c18c78d34bbe8f62c466ea217cba7ee57b82c80ac84be2d8ca4fb9d7d1918"
+  version "1.1.7"
+  sha256 "7a2627504c7a0c765f71c2233b4309ebe45ce2f1078edd8a15009828ed61be17"
 
   url "https://github.com/abaj8494/homebrew-typing-telemetry/releases/download/v#{version}/Typtel-#{version}.zip"
   name "Typtel"
   desc "Keystroke and mouse distance metrics for developers"
   homepage "https://github.com/abaj8494/typing-telemetry"
 
-  # Install the app to /Applications
   app "Typtel.app"
 
-  # Remove quarantine to prevent "app is damaged" error (unsigned app)
   postflight do
     system_command "/usr/bin/xattr",
                    args: ["-cr", "#{appdir}/Typtel.app"],
                    sudo: false
   end
 
-  # Symlink CLI to /usr/local/bin
   binary "Typtel.app/Contents/MacOS/typtel"
 
-  # Uninstall: stop service and remove LaunchAgent
   uninstall launchctl: "com.typtel.menubar"
 
   zap trash: [
@@ -51,6 +47,7 @@ cask "typtel" do
       typtel stats     - Show statistics
       typtel today     - Today's keystroke count
       typtel test      - Typing speed test
+      typtel v         - View charts in browser
 
     To start automatically at login, enable "Launch at Login" in the menu bar.
   EOS
